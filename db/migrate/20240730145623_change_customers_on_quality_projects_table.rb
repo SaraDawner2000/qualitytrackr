b5891629a1b8430314e9da049ea7002dab1f7600
Natalie Demyanenko <83845798+SaraDawner2000@users.noreply.github.com>
2024-07-30 10:07:14 -0500

    update quality project model to include enum

--- db/migrate/20240730145623_change_customers_on_quality_projects_table.rb
+++ db/migrate/20240730145623_change_customers_on_quality_projects_table.rb
@@ -1,7 +1,7 @@
 class ChangeCustomersOnQualityProjectsTable < ActiveRecord::Migration[7.0]
   def change
     execute <<-SQL
-      CREATE TYPE customers AS ENUM ('Sparky', 'McTracktor');
+      CREATE TYPE customers AS ENUM ('sparky', 'mctracktor');
     SQL
   end
   change_column :quality_projects, :customer, :customers
