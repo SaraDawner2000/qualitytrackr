afa9bab2e7781a77a6c30145a5ed9362de170913
Natalie Demyanenko <83845798+SaraDawner2000@users.noreply.github.com>
2024-08-01 16:19:38 -0500

    add and configure aws bucket

--- app/models/quality_project.rb
+++ app/models/quality_project.rb
@@ -1,4 +1,6 @@
 class QualityProject < ApplicationRecord
+  has_one_attached :inspection_plan
+  has_one_attached :assembled_record
   enum customer_options: { unready: "unready", ready: "ready", sent: "sent", approved: "approved", rejected: "rejected" }
   enum customers: { sparky: "sparky", mctracktor: "mctracktor" }
   validates :customer, presence: true
