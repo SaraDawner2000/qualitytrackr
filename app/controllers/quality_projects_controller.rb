2795f673f610c633e1662d961348f52fd42178b0
Natalie Demyanenko <83845798+SaraDawner2000@users.noreply.github.com>
2024-08-07 12:25:34 -0500

    add missed before_action to quality projects controller

--- app/controllers/quality_projects_controller.rb
+++ app/controllers/quality_projects_controller.rb
@@ -1,7 +1,7 @@
 class QualityProjectsController < ApplicationController
   include Cleanable
 
-  before_action :set_quality_project, only: %i[ show edit update destroy ]
+  before_action :set_quality_project, only: %i[ show edit update destroy remove_assembled_record remove_inspection_plan]
 
   # GET /quality_projects or /quality_projects.json
   def index
